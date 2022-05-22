14730c9aa9cd2f89fc2ec4a6c7a3e46d4de306e6
晨光黑苹果安装 <10492770+chenguangheiguo@user.noreply.gitee.com>
2022-05-22 02:20:47 +0000

    bump version:0.8.0

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+name="OpenCore-"
+srcFile="/"
+srcPath="./OpenCore"
+targetPath="./build"
+version=0.8.0
+date=$(date +%Y%m%d)
+cpu=-REL-
+
+if [ -d $srcPath/$srcFile ];then
+echo "打包目录存在"
+
+if [ -d $targetPath ];then
+tar -zcvf  $targetPath/$name$version$cpu$date.tar.gz --exclude=.DS_Store $srcPath/$srcFile
+else
+echo "打包目录不存在"
+fi
+echo "打包完成开始生成sha256sum码"
+fi
+if [ -d $targetPath ];then
+md5sum $targetPath/$name$version$cpu$date.tar.gz >$targetPath/$name$version$cpu$date.tar.gz.sha256sum.txt
+else
+echo "存放目录不存在"
+fi
